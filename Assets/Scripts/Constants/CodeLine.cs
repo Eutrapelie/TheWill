1537f843a2cc6d685ad2f04d8c52384bd3102b59
Ekzael <ekzael@gmail.com>
2017-04-02 20:36:32 +0200

    [Integration]  Abi + Geoffroy + Hippo niveau 1 : Finis

--- Assets/Scripts/Constants/CodeLine.cs
+++ Assets/Scripts/Constants/CodeLine.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public enum CodeLine
 {
-    DoNotMatterGeoffroy0000, //COMMENTAIRE
-    DoNotMatterGeoffroy0001,
+    DoNotMatterGeoffroy0000, //Première fois qu'on parle à Geoffroy - Jour 1 - Premier choix
+    DoNotMatterGeoffroy0001, // Second Choix
+    DoNotMatterGeoffroy0002, // Troisième Choix
+    DoNotMatterGeoffroy0003, // Troisième Choix - Contrat+
+
+
     DoNotMatterAbigail0000,//Première fois qu'on parle à Aby - Jour 1
-    DoNotMatterAbigail0001,// idem - Choix 1
-    DoNotMatterAbigail0002,// idem - Choix 2
+    DoNotMatterAbigail0001,// Intro Dialogue
+    DoNotMatterAbigail0002,// idem - Choix 1 et 2
     DoNotMatterAbigail0003,// idem - Choix 3
     DoNotMatterAbigail0004,// idem - Choix 4
-    DoNotMatterAbigail0005,// idem - Choix 5
+
+
+    DoNotMatterHippo0000,//Première fois qu'on parle à Hippo - Jour 1
+    DoNotMatterHippo0001,//
+    DoNotMatterHippo0002,//
 }
