da600808c89f73755696cf1808d907f1d7758ad7
Ekzael <ekzael@gmail.com>
2017-05-12 21:01:20 +0200

    [Intégration] Début Ernest + Geoffroy quasi complet // Rename de TOUTES les codelines, par encore intégrées

--- Assets/Scripts/Constants/CodeLine.cs
+++ Assets/Scripts/Constants/CodeLine.cs
@@ -4,42 +4,50 @@ using UnityEngine;
 
 public enum CodeLine
 {
-    DoNotMatterGeoffroy0000, //Première fois qu'on parle à Geoffroy - Jour 1 - Premier choix
-    DoNotMatterGeoffroy0001, // Second Choix
-    DoNotMatterGeoffroy0002, // Troisième Choix
-    DoNotMatterGeoffroy0003, // Troisième Choix - Contrat+
+    GeoffroyD1000, //Première fois qu'on parle à Geoffroy - Jour 1 - Premier choix
+    GeoffroyD1001, // Second Choix
+    GeoffroyD1002, // Troisième Choix
+    GeoffroyD1003, // Troisième Choix - Contrat+
+	GeoffroyD2001, // Avoir son contrat
+	GeoffroyD2002,
+	GeoffroyD2003, //Suite quête après contrat
 
 
-    DoNotMatterAbigail0000,//Première fois qu'on parle à Aby - Jour 1
-    DoNotMatterAbigail0001,// Intro Dialogue
-    DoNotMatterAbigail0002,// idem - Choix 1 et 2
-    DoNotMatterAbigail0003,// idem - Choix 3
-    DoNotMatterAbigail0004,// idem - Choix 4
+    AbigailD1000,//Première fois qu'on parle à Aby - Jour 1
+    AbigailD1001,// Intro Dialogue
+    AbigailD1002,// idem - Choix 1 et 2
+    AbigailD1003,// idem - Choix 3
+    AbigailD1004,// idem - Choix 4
 
 
-    DoNotMatterHippo0000,//Première fois qu'on parle à Hippo - Jour 1
-    DoNotMatterHippo0001,//
-    DoNotMatterHippo0002,//
+    HippoD1000,//Première fois qu'on parle à Hippo - Jour 1
+    HippoD1001,//
+    HippoD1002,//
 
 
 
-    DoNotMatterErnest0000,//Première fois qu'on parle à Ernest - Jour 1
-    DoNotMatterErnest0001,//Choix 1
-    DoNotMatterErnest0002,//Choix 2
-    DoNotMatterErnest0003,//Choix 3
-    DoNotMatterErnest0004,//Dial supp "Abi?"
+    ErnestD1000,//Première fois qu'on parle à Ernest - Jour 1
+    ErnestD1001,//Choix 1
+    ErnestD1002,//Choix 2
+    ErnestD1003,//Choix 3
+    ErnestD1004,//Dial supp "Abi?"
+	ErnestD2001,//Premier Dialogue J2
+	ErnestD2002,//Premier echec à l'ouverture du Phone
+	ErnestD2003,//
 
-    DoNotMatterOphelie0000,//Première fois qu'on parle à Ernest - Jour 1
-    DoNotMatterOphelie0001,//Choix 1
-    DoNotMatterOphelie0002,//Choix 2
-    DoNotMatterOphelie0003,//
+    OphelieD1000,//Première fois qu'on parle à Ernest - Jour 1
+    OphelieD1001,//Choix 1
+    OphelieD1002,//Choix 2
+    OphelieD1003,//
 	
 	
-    DoNotMatterLeontine0000,//Première fois qu'on parle à Leontine - Jour 1
-    DoNotMatterLeontine0001,//
-    DoNotMatterLeontine0002,//
-    DoNotMatterLeontine0003,//
-    DoNotMatterLeontine0004,//
-    DoNotMatterLeontine0005,//
-    DoNotMatterLeontine0006,//
+    LeontineD1000,//Première fois qu'on parle à Leontine - Jour 1
+    LeontineD1001,//
+    LeontineD1002,//
+    LeontineD1003,//
+    LeontineD1004,//
+    LeontineD1005,//
+    LeontineD1006,//
+	
+	KimD2001,//Première entrée cuisine
 }
