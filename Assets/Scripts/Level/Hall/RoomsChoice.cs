7466b473a53c2ed7e9bd4d6544b8476e228424b3
ONYX\Ewakhine <wenell@hotmail.fr>
2017-10-18 23:50:19 +0200

    [Scenes] + grid in Hall to change active room + button in LivingRoom to go to Hall + fade's animations when scene's transition

---
+++ Assets/Scripts/Level/Hall/RoomsChoice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheWill
+{
+    public class RoomsChoice : MonoBehaviour
+    {
+        [SerializeField] Canvas _fadeCanvas;
+        [SerializeField] AnimationClip _closingAnim;
+
+        
+        void Awake()
+        {
+            Debug.Assert(_fadeCanvas != null, "_fadeCanvas cannot be null.");
+            Debug.Assert(_closingAnim != null, "_closingAnim cannot be null.");
+        }
+
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+
+        public void Btn_ChangeRoom(string a_roomName)
+        {
+            _fadeCanvas.GetComponent<Animator>().SetTrigger("Closing");
+            StartCoroutine(LaunchClosingAndChangeRoomCoroutine(a_roomName));
+        }
+
+        IEnumerator LaunchClosingAndChangeRoomCoroutine(string a_roomName)
+        {
+            yield return new WaitForSeconds(_closingAnim.length);
+            SceneManager.LoadScene(a_roomName);
+        }
+
+    } // class
+} // namespace
