f2237703c018dbcaace04eec306187234129edd5
ONYX\Ewakhine <tristan.gassies@gmx.fr>
2017-12-02 18:14:51 +0100

    [Hall] + show/hide rommsChoicePanel [Backpack] Change button to open backpackPanel

--- Assets/Scripts/Level/Hall/RoomsChoice.cs
+++ Assets/Scripts/Level/Hall/RoomsChoice.cs
@@ -5,39 +5,64 @@ using UnityEngine.SceneManagement;
 
 namespace TheWill
 {
+    [RequireComponent(typeof(Animator))]
     public class RoomsChoice : MonoBehaviour
     {
         [SerializeField] Canvas _fadeCanvas;
         [SerializeField] AnimationClip _closingAnim;
 
+        Animator _animator;
         
+        
+    ///////////////////////////////////////////////////////////////
+    /// GENERAL FUNCTIONS /////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
         void Awake()
         {
             Debug.Assert(_fadeCanvas != null, "_fadeCanvas cannot be null.");
             Debug.Assert(_closingAnim != null, "_closingAnim cannot be null.");
-        }
 
-        void Start()
-        {
-
+            _animator = GetComponent<Animator>();
         }
+        /*********************************************************/
+
+        void Start()  { }
+        /*********************************************************/
+
+        void Update() { }
+        /*********************************************************/
         
-        void Update()
+    ///////////////////////////////////////////////////////////////
+    /// PRIVATE FUNCTIONS /////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
+        IEnumerator LaunchClosingAndChangeRoomCoroutine(string a_roomName)
         {
-
+            yield return new WaitForSeconds(_closingAnim.length);
+            SceneManager.LoadScene(a_roomName);
         }
+        /*********************************************************/
+        
+    ///////////////////////////////////////////////////////////////
+    /// PUBLIC FUNCTIONS //////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
+        public void SetVisibility(bool a_show)
+        {
+            _animator.SetBool("Show", a_show);
+        }
+        /*********************************************************/
+
+        public void Btn_CloseRoomsChoice()
+        {
+            SetVisibility(false);
+        }
+        /*********************************************************/
 
         public void Btn_ChangeRoom(string a_roomName)
         {
             _fadeCanvas.GetComponent<Animator>().SetTrigger("Closing");
             StartCoroutine(LaunchClosingAndChangeRoomCoroutine(a_roomName));
         }
-
-        IEnumerator LaunchClosingAndChangeRoomCoroutine(string a_roomName)
-        {
-            yield return new WaitForSeconds(_closingAnim.length);
-            SceneManager.LoadScene(a_roomName);
-        }
+        /*********************************************************/
 
     } // class
 } // namespace
--- Assets/Scripts/Level/LivingRoom/ActionsPanel.cs
+++ Assets/Scripts/Level/LivingRoom/ActionsPanel.cs
@@ -11,58 +11,86 @@ namespace TheWill
     public class ActionsPanel : MonoBehaviour, IPointerExitHandler
     {
         Animator _animator;
-        [SerializeField]
-        Backpack _backpack;
+        [SerializeField] Backpack _backpack;
+        [SerializeField] BackpackPanel _backpackPanel;
 
         [SerializeField] Canvas _fadeCanvas;
         [SerializeField] AnimationClip _closingAnim;
+        
+        [Tooltip("Let 'None' if the current room isn't 'Hall'")]
+        [SerializeField] RoomsChoice _roomsChoice;
 
         
+    ///////////////////////////////////////////////////////////////
+    /// GENERAL FUNCTIONS /////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
         void Awake()
         {
             _animator = GetComponent<Animator>();
+            Debug.Assert(_backpack != null, "_backpack cannot be null.");
+            Debug.Assert(_backpackPanel != null, "_backpackPanel cannot be null.");
 
             Debug.Assert(_fadeCanvas != null, "_fadeCanvas cannot be null.");
             Debug.Assert(_closingAnim != null, "_closingAnim cannot be null.");
+
+            if (SceneManager.GetActiveScene().name == "Hall")
+                Debug.Assert(_roomsChoice != null, "_roomsChoice cannot be null.");
         }
-        
+        /*********************************************************/
+
         void Start()
         {
             _backpack.OnMouseEnter += OnMouseEnterBackpack;
         }
+        /*********************************************************/
+
+        void Update() { }
+        /*********************************************************/
         
-        void Update()
-        {
-
-        }
-
+    ///////////////////////////////////////////////////////////////
+    /// PRIVATE FUNCTIONS /////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
         void OnMouseEnterBackpack()
         {
             _animator.SetBool("Show", true);
         }
+        /*********************************************************/
 
         IEnumerator LaunchClosingAndChangeRoomCoroutine(string a_roomName)
         {
             yield return new WaitForSeconds(_closingAnim.length);
             SceneManager.LoadScene(a_roomName);
         }
-
+        /*********************************************************/
+        
+    ///////////////////////////////////////////////////////////////
+    /// PUBLIC FUNCTIONS //////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
         public void Btn_ClickOnBackpack()
         {
-            // Open BackpackPanel
+            _backpackPanel.SetVisibility(true);
         }
+        /*********************************************************/
 
         public void Btn_ChangeRoom(string a_roomName)
         {
             _fadeCanvas.GetComponent<Animator>().SetTrigger("Closing");
             StartCoroutine(LaunchClosingAndChangeRoomCoroutine(a_roomName));
         }
+        /*********************************************************/
+
+        public void Btn_OpenRoomsChoice()
+        {
+            _roomsChoice.SetVisibility(true);
+        }
+        /*********************************************************/
 
         #region Interface
         public void OnPointerExit(PointerEventData a_eventData)
         {
             _animator.SetBool("Show", false);
         }
+        /*********************************************************/
         #endregion
     } // class
 } // namespace
