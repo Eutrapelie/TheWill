75555ff2bf09ef66e583d84721f5ca0f52dc19f9
ONYX\Ewakhine <tristan.gassies@gmx.fr>
2018-12-15 17:51:05 +0100

    [TASK] +ChangeDay

---
+++ Assets/Scripts/BehaviourTree/CustomTask/ChangeDay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using NodeCanvas;
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheWill
+{
+    [Category("♥ The Will")]
+    public class ChangeDay : ActionTask
+    {
+        public int day;
+
+
+        protected override string info
+        {
+            get
+            {
+                return string.Format("Change to day {0}", day);
+            }
+        }
+
+        protected override void OnExecute()
+        {
+            Debug.Log("[ChangeDay] " + day);
+            Game.Current.dayNumber = day;
+            EndAction(true);
+        }
+    }
+}
