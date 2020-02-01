49df795fb8eb90fa81904e3a0de0a9a3027e23e9
Ekzael <ekzael@gmail.com>
2020-02-01 16:22:33 +0100

    [Integration] Behavior Tree Cuisine Jour 2

--- Assets/Scripts/Constants/RoomSpot.cs
+++ Assets/Scripts/Constants/RoomSpot.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum RoomSpot {
-    LivingRoomSpot1,
-    LivingRoomSpot2,
-    LivingRoomSpot3,
-    LivingRoomSpot4,
-    LivingRoomSpot5,
-    LivingRoomSpot6,
-    LivingRoomUpSpot1,
 
-    GardenSpot1,
-    GardenSpot2,
 
-    EntranceSpot1,
-    EntranceSpot2,
+	AbigailsroomSpot1,
+	AbigailsroomSpot2,
 
-    HallSpot1,
-    HallSpot2
+	EntranceSpot1,
+	EntranceSpot2,
+
+	HallSpot1,
+	HallSpot2,
+
+	HippolytesroomSpot1,
+	HippolytesroomSpot2,
+
+	GardenSpot1,
+	GardenSpot2,
+
+	KitchenSpot1,
+	KitchenSpot2,
+
+	LivingRoomSpot1,
+	LivingRoomSpot2,
+	LivingRoomSpot3,
+	LivingRoomSpot4,
+	LivingRoomSpot5,
+	LivingRoomSpot6,
+	LivingRoomUpSpot1,
+
+	ToyroomSpot1,
+	ToyroomSpot2
+
 }
